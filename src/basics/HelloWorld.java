19d276cb89a500af3c40a75a451f3aa59f412fa1
strevizo <deri411@msn.com>
2020-04-05 15:57:47 -0500

    mejor HelloWorld

--- src/basics/HelloWorld.java
+++ src/basics/HelloWorld.java
@@ -3,7 +3,7 @@ package basics;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
-		System.out.println("I'm adding code to my project");
-		System.out.println("Adding code from GitHub");
+		System.out.println("Sergio rules");
+		System.out.println("Vichy sucks");
 	}
 }

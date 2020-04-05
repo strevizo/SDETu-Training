16f49d85fabdc691638824312032b47a651fb606
strevizo <deri411@msn.com>
2020-04-05 16:22:43 -0500

    Vichy rulea, tambien

--- src/basics/HelloWorld.java
+++ src/basics/HelloWorld.java
@@ -4,6 +4,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 		System.out.println("Sergio rules");
-		System.out.println("Vichy sucks");
+		System.out.println("Vichy rules, too");
 	}
 }
